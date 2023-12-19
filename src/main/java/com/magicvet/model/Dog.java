1c1581d161ff3c8b760786c4f1637c7e20b8fc45
jeck98 <helpit.odessa@gmail.com>
2023-12-19 08:02:22 +0200

    REQ-04: Show client info (with pet and dog)

--- src/main/java/com/magicvet/model/Dog.java
+++ src/main/java/com/magicvet/model/Dog.java
@@ -1,5 +1,7 @@
 package main.java.com.magicvet.model;
 
+import java.util.Objects;
+
 public class Dog extends Pet {
 
     private String size;
@@ -13,6 +15,21 @@ public class Dog extends Pet {
                 + ", size = " + size
                 + " }";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(size, dog.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), size);
+    }
+
     public void setSize(String size) {
         this.size = size;
     }
